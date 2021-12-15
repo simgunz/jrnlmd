7c8a3c8a6f8ea133f1c8628cd2ca8ca14d1eef03
Simone Gaiarin <simgunz@gmail.com>
2021-12-15 11:29:33 +0100

    Terminate notes with newline

--- tests/test_md_conversion.py
+++ tests/test_md_conversion.py
@@ -202,19 +202,19 @@ class TestParseNote(unittest.TestCase):
     def test_parse_one_line_note_no_dash(self):
         note = "a note"
         parsed_note = parse_note(note)
-        self.assertEqual("- a note", parsed_note)
+        self.assertEqual("- a note\n", parsed_note)
 
     def test_parse_one_line_note_with_dash(self):
         note = "- a note"
         parsed_note = parse_note(note)
-        self.assertEqual("- a note", parsed_note)
+        self.assertEqual("- a note\n", parsed_note)
 
     def test_multiline_note(self):
         note = "- a note\n- with two lines"
         parsed_note = parse_note(note)
-        self.assertEqual("- a note\n- with two lines", parsed_note)
+        self.assertEqual("- a note\n- with two lines\n", parsed_note)
 
     def test_multiline_note_without_dash(self):
         note = "a note\n- with two lines"
         parsed_note = parse_note(note)
-        self.assertEqual("a note\n- with two lines", parsed_note)
+        self.assertEqual("a note\n- with two lines\n", parsed_note)
