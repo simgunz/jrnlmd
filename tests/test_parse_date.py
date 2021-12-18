33b364a8190d6747f6016d82e75c93f1589d6388
Simone Gaiarin <simone.gaiarin@trovaprezzi.it>
2021-12-18 10:00:23 +0100

    Rename some tests

--- tests/test_parse_date.py
+++ tests/test_parse_date.py
@@ -1,19 +1,19 @@
 from jrnlmd.jrnlmd import parse_date
 
 
-def test_date_parser():
+def test_parse_date():
     txt_date = "12nov2021"
     iso_date = parse_date(txt_date)
     assert "2021-11-12" == iso_date
 
 
-def test_date_parser_unparsable():
+def test_parse_date_unparsable():
     txt_date = "aaaa"
     iso_date = parse_date(txt_date)
     assert None == iso_date
 
 
-def test_date_parser_single_char():
+def test_parse_date_single_char():
     txt_date = "a"
     iso_date = parse_date(txt_date)
     assert None == iso_date
