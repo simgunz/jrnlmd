78e907959bfa242e0fecf4e6176de651bff38915
Simone Gaiarin <simone.gaiarin@trovaprezzi.it>
2021-12-18 09:58:39 +0100

    Use pytest for all tests

--- tests/test_dict_to_md.py
+++ tests/test_dict_to_md.py
@@ -1,32 +1,30 @@
-import unittest
-
 from jrnlmd.jrnlmd import dict_to_md
 
 
-class TestDictToMdConversion(unittest.TestCase):
-    def test_dict_to_md_one_level(self):
-        d = {"2021-01-01": {"topic1": "- first line\n- second line\n"}}
-        text = dict_to_md(d)
-        self.assertEqual(
-            """# 2021-01-01
+def test_dict_to_md_one_level():
+    d = {"2021-01-01": {"topic1": "- first line\n- second line\n"}}
+    text = dict_to_md(d)
+    assert (
+        """# 2021-01-01
 
 ## topic1
 
 - first line
 - second line
-""",
-            text,
-        )
+"""
+        == text
+    )
 
-    def test_dict_to_md_comment_in_code_fence(self):
-        d = {
-            "2021-01-01": {
-                "topic1": "- first line\n  wrapped\n- second line\n  ```bash\n  # comment\n  sudo pacman -S bash\n  ```\n"
-            }
+
+def test_dict_to_md_comment_in_code_fence():
+    d = {
+        "2021-01-01": {
+            "topic1": "- first line\n  wrapped\n- second line\n  ```bash\n  # comment\n  sudo pacman -S bash\n  ```\n"
         }
-        text = dict_to_md(d)
-        self.assertEqual(
-            """# 2021-01-01
+    }
+    text = dict_to_md(d)
+    assert (
+        """# 2021-01-01
 
 ## topic1
 
@@ -37,21 +35,22 @@ class TestDictToMdConversion(unittest.TestCase):
   # comment
   sudo pacman -S bash
   ```
-""",
-            text,
-        )
+"""
+        == text
+    )
 
-    def test_dict_to_md_two_levels(self):
-        d = {
-            "2021-01-01": {"topic1": "- first line\n- second line\n"},
-            "2021-01-02": {
-                "topic2": "- third line\n",
-                "topic3": "- fourth line\n- fifth line\n",
-            },
-        }
-        text = dict_to_md(d)
-        self.assertEqual(
-            """# 2021-01-01
+
+def test_dict_to_md_two_levels():
+    d = {
+        "2021-01-01": {"topic1": "- first line\n- second line\n"},
+        "2021-01-02": {
+            "topic2": "- third line\n",
+            "topic3": "- fourth line\n- fifth line\n",
+        },
+    }
+    text = dict_to_md(d)
+    assert (
+        """# 2021-01-01
 
 ## topic1
 
@@ -68,6 +67,6 @@ class TestDictToMdConversion(unittest.TestCase):
 
 - fourth line
 - fifth line
-""",
-            text,
-        )
+"""
+        == text
+    )
