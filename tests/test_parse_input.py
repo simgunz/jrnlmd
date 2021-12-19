21a44f826285a05603b168fb1ac3ea11d0b9e8fe
Simone Gaiarin <simgunz@gmail.com>
2021-12-19 14:18:05 +0100

    Require dot after date

--- tests/test_parse_input.py
+++ tests/test_parse_input.py
@@ -20,7 +20,7 @@ def test_input_single_note(today):
 
 
 def test_input_single_note_with_date():
-    txt_input = "12nov2021 a note"
+    txt_input = "12nov2021 . a note"
     date, topic, note = parse_input(txt_input)
     assert "2021-11-12" == date
     assert "ungrouped" == topic
@@ -36,7 +36,7 @@ def test_input_single_note_with_topic(today):
 
 
 def test_input_single_note_with_date_and_topic():
-    txt_input = "12nov2021 topic1 is this . a note"
+    txt_input = "12nov2021 . topic1 is this . a note"
     date, topic, note = parse_input(txt_input)
     assert "2021-11-12" == date
     assert "topic1 is this" == topic
@@ -44,7 +44,9 @@ def test_input_single_note_with_date_and_topic():
 
 
 def test_input_multiple_notes_with_date_and_topic():
-    txt_input = "12nov2021 topic1 is this . first bullet , second bullet , third bullet"
+    txt_input = (
+        "12nov2021 . topic1 is this . first bullet , second bullet , third bullet"
+    )
     date, topic, note = parse_input(txt_input)
     assert "2021-11-12" == date
     assert "topic1 is this" == topic
