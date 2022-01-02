cf293d110d96d5311f63ac38b7f8ec66f19b1094
Simone Gaiarin <simgunz@gmail.com>
2022-01-02 09:48:26 +0100

    Add function to filter topic

--- tests/test_filter_dict.py
+++ tests/test_filter_dict.py
@@ -1,4 +1,4 @@
-from jrnlmd.jrnlmd import filter_dict_date, md_to_dict
+from jrnlmd.jrnlmd import filter_dict_date, filter_dict_topic, md_to_dict
 
 
 def test_filter_dict_equal_date(journal_multidate):
@@ -11,6 +11,16 @@ def test_filter_dict_since_date(journal_multidate):
     journal_dict = md_to_dict(journal_multidate.read_text())
     filtered_dict = filter_dict_date(journal_dict, "2021-11-05", str.__ge__)
     assert {
+        "2021-11-05": {"topic1": "- second date note\n"},
+        "2021-11-10": {"topic1": "- third date note\n"},
+    } == filtered_dict
+
+
+def test_filter_dict_topic(journal_multidate):
+    journal_dict = md_to_dict(journal_multidate.read_text())
+    filtered_dict = filter_dict_topic(journal_dict, "topic1")
+    assert {
+        "2021-11-01": {"topic1": "- another note\n"},
         "2021-11-05": {"topic1": "- second date note\n"},
         "2021-11-10": {"topic1": "- third date note\n"},
     } == filtered_dict
