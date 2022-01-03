7726a6aaed83b9080e9308acb3e7142abae0b9a8
Simone Gaiarin <simgunz@gmail.com>
2022-01-03 14:27:35 +0100

    Add method to convert journal to md (ported single functions)

--- jrnlmd/journal.py
+++ jrnlmd/journal.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Set
 
 from .usertypes import JDict, JDictDDateDTopic
 
@@ -16,3 +17,27 @@ class Journal:
 
     def _empty_dict(self):
         return defaultdict(lambda: defaultdict(str))
+
+    def to_md(
+        self,
+        date_descending: bool = True,
+        simplified: bool = False,
+        compact: bool = False,
+    ) -> str:
+        keys: Set[str] = set()
+        for v in self._j.values():
+            keys.update(v.keys())
+        canSimplify = len(keys) == 1
+        output = []
+        for day in sorted(self._j, reverse=date_descending):
+            output.append(f"# {day}")
+            if not compact:
+                output.append("")
+            for topic in self._j[day]:
+                if not (simplified and canSimplify):
+                    output.append(f"## {topic}")
+                    if not compact:
+                        output.append("")
+                note = self._j[day][topic]
+                output.append(note)
+        return "\n".join(output)
