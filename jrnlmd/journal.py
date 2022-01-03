67d8851c80b48ae7f39005237666e9a819252116
Simone Gaiarin <simgunz@gmail.com>
2022-01-03 14:12:36 +0100

    Create Journal from dictionary

--- jrnlmd/journal.py
+++ jrnlmd/journal.py
@@ -1,8 +1,18 @@
 from collections import defaultdict
 
-from .usertypes import JDictDDateDTopic
+from .usertypes import JDict, JDictDDateDTopic
 
 
 class Journal:
+    @classmethod
+    def from_dict(cls, dictionary: JDict):
+        journal = cls()
+        for date, v in dictionary.items():
+            journal._j[date] = defaultdict(str, v)
+        return journal
+
     def __init__(self):
-        self._j: JDictDDateDTopic = defaultdict(lambda: defaultdict(str))
+        self._j: JDictDDateDTopic = self._empty_dict()
+
+    def _empty_dict(self):
+        return defaultdict(lambda: defaultdict(str))
--- tests/test_journal.py
+++ tests/test_journal.py
@@ -1,6 +1,22 @@
+from collections import defaultdict
+
 from jrnlmd.journal import Journal
 
 
 def test_create_empty_journal():
     journal = Journal()
-    assert journal._j is not None
+    assert isinstance(journal._j, defaultdict)
+
+
+def test_from_dict():
+    d = {
+        "2021-01-01": {"topic1": "- first line\n- second line\n"},
+        "2021-01-02": {
+            "topic2": "- third line\n",
+            "topic3": "- fourth line\n- fifth line\n",
+        },
+    }
+    journal = Journal.from_dict(d)
+    assert d == journal._j
+    assert isinstance(journal._j, defaultdict)
+    assert isinstance(journal._j["2021-01-01"], defaultdict)
