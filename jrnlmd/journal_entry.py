f55f374f4cf75ff47fbae5ab80fa67c612003b88
Simone Gaiarin <simone.gaiarin@trovaprezzi.it>
2022-01-07 15:16:26 +0100

    Parse note in journal entry

--- jrnlmd/journal_entry.py
+++ jrnlmd/journal_entry.py
@@ -4,7 +4,14 @@ UNDEFINED_TOPIC = "ungrouped"
 
 
 class JournalEntry:
+    @staticmethod
+    def _parse_note(note: str) -> str:
+        stripped_note = note.removeprefix("-").lstrip()
+        parsed_note = f"- {stripped_note}"
+        rstripped_parsed_note = parsed_note.rstrip("\n")
+        return f"{rstripped_parsed_note}\n"
+
     def __init__(self, note, date: str = None, topic: str = None) -> None:
-        self.note = note
+        self.note = self._parse_note(note)
         self.date = date or datetime.date.today().isoformat()
         self.topic = topic or UNDEFINED_TOPIC
