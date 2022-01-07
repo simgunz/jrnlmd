e84c8087fcd8f9230d1e0bfedeac53050e6d11e6
Simone Gaiarin <simgunz@gmail.com>
2022-01-07 16:45:03 +0100

    Add clone of parse_input that returns list of str for the note

--- jrnlmd/parsers.py
+++ jrnlmd/parsers.py
@@ -78,6 +78,41 @@ def join_notes_tokens(notes_tokens: List[List[str]]) -> str:
     return "".join((parse_note(note) for note in notes))
 
 
+def parse_journal_entry_text(
+    text: str,
+) -> Tuple[Optional[str], Optional[str], Optional[List[str]]]:
+    """Parse a journal entry text and return date, topic, and a list of notes.
+
+    Parameters
+    ----------
+    text : str
+        Texual input.
+
+    Returns
+    -------
+    Tuple[Optional[str], Optional[str], Optional[List[str]]]
+        date, topic, notes
+    """
+    m = re.search(":(?: |$)", text)
+    colon_pos = m.start() if m else -1
+    date = parse_date(text[:colon_pos]) if colon_pos > 0 else None
+    text = text[colon_pos + 1 :].strip()
+    if not text:
+        return date, None, None
+    tokens = text.split()
+    maybe_topic_notes_tokens = split_list_on_delimiter(tokens, TOKEN_SEP)
+    topic = " ".join(maybe_topic_notes_tokens[0])
+    if len(maybe_topic_notes_tokens) == 1:
+        return date, topic, None
+    elif len(maybe_topic_notes_tokens) == 2:
+        notes_tokens = maybe_topic_notes_tokens[-1]
+        split_notes_tokens = split_list_on_delimiter(notes_tokens, NOTE_SEP)
+        notes = [" ".join(tokens) for tokens in split_notes_tokens]
+        return date, topic, notes
+    else:
+        raise ValueError(f"Too many {TOKEN_SEP} in input.")
+
+
 def split_list_on_delimiter(tokens: List[str], delimiter: str) -> List[List[str]]:
     return [
         list(y) for x, y in itertools.groupby(tokens, lambda z: z == delimiter) if not x
