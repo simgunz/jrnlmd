2c5b3af44c3c42a6594971c442e39431f7df0178
Simone Gaiarin <simgunz@gmail.com>
2022-01-07 13:59:04 +0100

    Add method to filter on topic

--- tests/test_journal_filter.py
+++ tests/test_journal_filter.py
@@ -6,3 +6,26 @@ def test_journal_on_date(journal_multidate):
 def test_journal_on_date_not_present(journal_multidate):
     journal_filtered = journal_multidate.on("1999-11-05")
     assert {} == journal_filtered._j
+
+
+def test_journal_about_topic(journal_multidate):
+    journal_filtered = journal_multidate.about("topic1")
+    assert {
+        "2021-11-01": {"topic1": "- another note\n"},
+        "2021-11-05": {"topic1": "- second date note\n"},
+        "2021-11-10": {"topic1": "- third date note\n"},
+    } == journal_filtered._j
+
+
+def test_journal_about_topic_not_present(journal_multidate):
+    journal_filtered = journal_multidate.about("nothing")
+    assert {} == journal_filtered._j
+
+
+def test_journal_about_topic_partial_matching(journal_multidate):
+    journal_filtered = journal_multidate.about("pic1")
+    assert {
+        "2021-11-01": {"topic1": "- another note\n"},
+        "2021-11-05": {"topic1": "- second date note\n"},
+        "2021-11-10": {"topic1": "- third date note\n"},
+    } == journal_filtered._j
