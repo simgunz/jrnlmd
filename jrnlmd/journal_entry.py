6c181ea437dee191a0f252f55208c8bc2abd4edf
Simone Gaiarin <simone.gaiarin@trovaprezzi.it>
2022-01-07 16:05:37 +0100

    note can be empty in journal entry

--- jrnlmd/journal_entry.py
+++ jrnlmd/journal_entry.py
@@ -1,6 +1,6 @@
 import datetime
 import re
-from typing import Union
+from typing import Optional, Union
 
 import dateparser
 
@@ -9,7 +9,9 @@ UNDEFINED_TOPIC = "ungrouped"
 
 class JournalEntry:
     @staticmethod
-    def _parse_note(note: str) -> str:
+    def _parse_note(note: Optional[str]) -> str:
+        if note is None:
+            return ""
         stripped_note = note.removeprefix("-").lstrip()
         parsed_note = f"- {stripped_note}"
         rstripped_parsed_note = parsed_note.rstrip("\n")
@@ -39,7 +41,7 @@ class JournalEntry:
             return None
         return a_date.date().isoformat()
 
-    def __init__(self, note, date: str = None, topic: str = None) -> None:
+    def __init__(self, note: str = None, date: str = None, topic: str = None) -> None:
         self.note = self._parse_note(note)
         self.date = date or datetime.date.today().isoformat()
         self.topic = topic or UNDEFINED_TOPIC
