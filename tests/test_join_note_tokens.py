db8ec985a74edc2aeca4d903f3ce03487f101297
Simone Gaiarin <simone.gaiarin@trovaprezzi.it>
2022-01-07 16:18:40 +0100

    Group parses test in a single test

--- tests/test_join_note_tokens.py
+++
@@ -1,7 +0,0 @@
-from jrnlmd.parsers import join_notes_tokens
-
-
-def test_join_notes_tokens():
-    notes_tokens = [["word1"], ["word2", "word3"]]
-    result = join_notes_tokens(notes_tokens)
-    assert "- word1\n- word2 word3\n" == result
