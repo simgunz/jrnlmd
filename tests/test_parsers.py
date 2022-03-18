ee080c9bb726ea87208f154a2df3b8da1da47952
Simone Gaiarin <simgunz@gmail.com>
2022-03-18 19:04:55 +0100

    Add tests for split on delimiter whitout surrounding spaces

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -85,3 +85,15 @@ def test_split_with_one_delimiter():
     text = "word1 . word2 word3"
     result = split_on_separator(text, sep=".")
     assert ["word1", "word2 word3"] == result
+
+
+def test_split_with_delimiter_not_surrounded_by_left_space():
+    text = "word1. word2 word3"
+    result = split_on_separator(text, sep=".")
+    assert ["word1. word2 word3"] == result
+
+
+def test_split_with_delimiter_not_surrounded_by_right_space():
+    text = "word1 .word2 word3"
+    result = split_on_separator(text, sep=".")
+    assert ["word1 .word2 word3"] == result
