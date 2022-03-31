5ea10d974f30f6aeef647d04b0dabead6d962b23
Simone Gaiarin <simgunz@gmail.com>
2022-03-31 18:56:21 +0200

    Refactor: rename test

--- tests/test_command_top.py
+++ tests/test_command_top.py
@@ -3,7 +3,7 @@ from click.testing import CliRunner
 from jrnlmd import jrnlmd
 
 
-def test_cat_full_journal(journal_multidate_file):
+def test_top(journal_multidate_file):
     runner = CliRunner()
     result = runner.invoke(jrnlmd.cli, ["-j", str(journal_multidate_file), "top"])
     assert (
